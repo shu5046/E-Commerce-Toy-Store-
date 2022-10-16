8825fed299383e4ee1e92d3b6591af5914aa150c
shu5046 <66690056+shu5046@users.noreply.github.com>
2022-10-16 18:29:42 +0530

    Add files via upload

---
+++ Ecommerce.Model/EntityFrameWork/EnityFramWorkDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Model.Dto;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Model.EntityFrameWork
+{
+    public partial class EnityFramWorkDbContext : DbContext
+    {
+        public EnityFramWorkDbContext(DbContextOptions<EnityFramWorkDbContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        public virtual DbSet<BasketItem> BasketItems { get; set; }
+
+        public virtual DbSet<MyUser> MyUsers { get; set; }
+
+        public virtual DbSet<InvoiceMaster> InvoiceMasters { get; set; }
+    }
+}
